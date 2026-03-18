d237aec7c0b4018c04512114b1b91a2588382cde
Ritesh0515 <karlestudy496@gmail.com>
2026-03-18 13:02:12 +0530

    Initial commit - REST API project (in progress)

---
+++ script.js
@@ -0,0 +1,26 @@
+const countriesContainer = document.querySelector('.countries-container')
+
+fetch(
+  'https://restcountries.com/v3.1/all?fields=name,capital,population,flags,region,subregion,languages,currencies,borders,cca3',
+)
+  .then((res) => res.json())
+  .then((data) => {
+    data.forEach((country) => {
+      console.log(country);
+      const countryCard = document.createElement('a')
+      countryCard.classList.add('country-card')
+      countryCard.href = `/country.html?name=${country.name.common}`
+
+      countryCard.innerHTML = `
+            <img src=${country.flags.svg} alt=${country.flags.alt}>
+            <div class="card-text">
+                <h3 class="card-title">${country.name.common}</h3>
+                <p><b>Population: </b>${country.population.toLocaleString('en-IN')}</p>
+                <p><b>Region: </b>${country.region}</p>
+                <p><b>Capital: </b>${country.capital}</p>
+            </div>
+`
+
+      countriesContainer.append(countryCard)
+    })
+  })
